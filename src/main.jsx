fc3e296443f0cb7c8307ce27c76dfc62e6510125
yg1110 <younggil9488@gmail.com>
2026-01-06 22:44:47 +0900

    feat: add locator

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import setupLocatorUI from "@locator/runtime";
+
+if (import.meta.env.NODE_ENV === "development") {
+  setupLocatorUI();
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
